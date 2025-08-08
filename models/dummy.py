269c6fa00b6517de0cdf2602ed3047e730699ab3
Nikos Arechiga <nikos.arechiga@gmail.com>
2025-08-07 21:08:14 -0700

    Renamed datasets folder to avoid conflict

--- models/dummy.py
+++ models/dummy.py
@@ -1,14 +1,16 @@
 # A dummy model, for testing
 
+import torch
 import torch.nn as nn
 
 class DummyModel(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super().__init__()
         self.linear = nn.Linear(num_inputs, num_outputs)
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
+        x = torch.flatten(x, start_dim=1) # don't flatten batch dim
         x = self.linear(x)
         x = self.softmax(x)
         return x
