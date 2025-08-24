128cc4ffe7f2ba17b6b2dd64db922864be9a2b4c
Nikos Arechiga <nikos.arechiga@gmail.com>
2025-08-23 20:31:57 -0700

    Reworked vision transformer with early exits

--- models/vision.py
+++ models/vision.py
@@ -1,62 +1,92 @@
 import torch.nn.functional as F
 
-def attach_aux_heads(model, exit_layers, num_classes, drop_rate=0.0):
-    model.aux_layers = exit_layers
-    model.aux_heads = nn.ModuleDict()
 
-    for layer in exit_layers:
-        model.aux_heads[str(layer)] = nn.Sequential(
-            nn.LayerNorm(model.embed_dim),
-            nn.Dropout(drop_rate),
-            nn.Linear(model.embed_dim, num_classes)
-        )
-    return model
+class ViTWithAuxHeads(nn.Module):
+    def __init__(self, model_name, num_classes,
+                 aux_layers=3,6,9], pretrained=True):
+        super.__init__()
+        self.vit = timm.create_model(model_name, pretrained=pretrained)
+        self.embed_dim = base_model.embed_dim
 
-@torch.no_grad()
-def predict_with_early_exit(model,
-                            x,
-                            exit_layers,
-                            threshold=0.9):
+        # TODO: swap out other parts?
+        self.vit.head = nn.Linear(self.embed_dim, num_classes)
+        self.aux_layers = aux_layers
+        self.aux_heads = nn.ModuleDict()
+        
+        for layer in self.aux_layers:
+            vit.aux_heads[str(layer)] = nn.Sequential(
+                nn.LayerNorm(self.embed_dim),
+                nn.Dropout(drop_rate),
+                nn.Linear(self.embed_dim, num_classes)
+            )
+
+    def forward(self, x):
+        """
+        We follow the form of the forward function of timm's ViT,
+        specifically the "forward_features" function.
+        https://github.com/huggingface/pytorch-image-models/blob/954613a470652e4a113ff45b62dbd15c4e229218/timm/models/vision_transformer.py#L934C9-L934C25
+        """
+        B = x.shape[0]
+        x = self.vit.patch_embed(x)
+        x = self.vit._pos_embed(x)
+        x = self.vit.patch_drop(x)
+        x = self.vit.norm_pre(x)
+
+        aux_logits = {}
+        for i, block in enumerate(self.vit.blocks, start=1):
+            x = block(x)
+            if i in self.aux_layers:
+                cls_rep = x[:,0] # the quirky CLS token
+                aux_logits[str(i)] = self.aux_heads[str(i)](cls_rep):
+
+        main_logits = self.vit.forward_head(x)
+
+        return main_logits, aux_logits
+        
+        
+
+    @torch.no_grad()
+    def predict_with_early_exit(x,
+                                threshold=0.9):
+        """
+        Takes a timm ViT model and rRuns inference with early exits.
+        - exit layers: list of layer indices where aux heads are attached.
+        - threshold: confidence threshold for early exit
     
-    """
-    Takes a timm ViT model and runs inference with early exits.
-    - exit layers: list of layer indices where aux heads are attached.
-    - threshold: confidence threshold for early exit
-
-    Returns:
-    - predicted class
-    - exit layer, or "final"
-    - confidence score
-
-    We follow the form of the forward function of timm's ViT,
-    specifically the "forward_features" function.
-    https://github.com/huggingface/pytorch-image-models/blob/954613a470652e4a113ff45b62dbd15c4e229218/timm/models/vision_transformer.py#L934C9-L934C25
-    """
-    x = model.patch_embed(x)
-    x = model._pos_embed(x)
-    x = model.patch_drop(x)
-    x = model.norm_pre(x)
-
-    # We skip the case of attention masking, not needed for
-    # currently planned experiments.
-    for i, block in enumerate(model.blocks, start=1):
-        x = block(x)
-
-        if i in exit_layers:
-            # It seems we need to do this because it's the only
-            # available representation at intermediate depths
-            cls_rep = x[:,0] # the quirky CLS token
-            logits = model.aux_heads[str(i)](cls_rep)
-            probs = F.softmax(logits, dim=-1)
-            confidence, prediction = probs.max(dim=-1)
-
-            if confidence.item() >= threshold:
-                return prediction.item(), i, confidence.item()
-
-    # If we didn't exit early...
-    logits = model.forward_head(x)
-    probs = F.softmax(logits, dim=-1)
-    confidence, prediction = probs.max(dim=-1)
-
-    return prediction.item(), "final", confidence.item()
+        Returns:
+        - predicted class
+        - exit layer, or "final"
+        - confidence score
+    
+        We follow the form of the forward function of timm's ViT,
+        specifically the "forward_features" function.
+        https://github.com/huggingface/pytorch-image-models/blob/954613a470652e4a113ff45b62dbd15c4e229218/timm/models/vision_transformer.py#L934C9-L934C25
+        """
+        x = self.vit.patch_embed(x)
+        x = self.vit._pos_embed(x)
+        x = self.vit.patch_drop(x)
+        x = self.vit.norm_pre(x)
+    
+        # We skip the case of attention masking, not needed for
+        # currently planned experiments.
+        for i, block in enumerate(self.vit.blocks, start=1):
+            x = block(x)
+    
+            if i in exit_layers:
+                # It seems we need to do this because it's the only
+                # available representation at intermediate depths
+                cls_rep = x[:,0] # the quirky CLS token
+                logits = self.vit.aux_heads[str(i)](cls_rep)
+                probs = F.softmax(logits, dim=-1)
+                confidence, prediction = probs.max(dim=-1)
+    
+                if confidence.item() >= threshold:
+                    return prediction.item(), i, confidence.item()
+    
+        # If we didn't exit early...
+        logits = self.vit.forward_head(x)
+        probs = F.softmax(logits, dim=-1)
+        confidence, prediction = probs.max(dim=-1)
+    
+        return prediction.item(), "final", confidence.item()
     
