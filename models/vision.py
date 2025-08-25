80727dc3df910707eb35294cdf43ff1f0db7aca8
Nikos Arechiga <nikos.arechiga@gmail.com>
2025-08-25 09:49:42 -0700

    Added a forward loss method

--- models/vision.py
+++ models/vision.py
@@ -2,8 +2,9 @@ import torch.nn.functional as F
 
 
 class ViTWithAuxHeads(nn.Module):
-    def __init__(self, model_name, num_classes,
-                 aux_layers=3,6,9], pretrained=True):
+    def __init__(
+        self, model_name, num_classes, aux_layers=[3, 6, 9], pretrained=True
+    ):
         super.__init__()
         self.vit = timm.create_model(model_name, pretrained=pretrained)
         self.embed_dim = base_model.embed_dim
@@ -12,12 +13,12 @@ class ViTWithAuxHeads(nn.Module):
         self.vit.head = nn.Linear(self.embed_dim, num_classes)
         self.aux_layers = aux_layers
         self.aux_heads = nn.ModuleDict()
-        
+
         for layer in self.aux_layers:
             vit.aux_heads[str(layer)] = nn.Sequential(
                 nn.LayerNorm(self.embed_dim),
                 nn.Dropout(drop_rate),
-                nn.Linear(self.embed_dim, num_classes)
+                nn.Linear(self.embed_dim, num_classes),
             )
 
     def forward(self, x):
@@ -32,32 +33,52 @@ class ViTWithAuxHeads(nn.Module):
         x = self.vit.patch_drop(x)
         x = self.vit.norm_pre(x)
 
-        aux_logits = {}
+        logits_dict = {}
         for i, block in enumerate(self.vit.blocks, start=1):
             x = block(x)
             if i in self.aux_layers:
-                cls_rep = x[:,0] # the quirky CLS token
-                aux_logits[str(i)] = self.aux_heads[str(i)](cls_rep):
+                cls_rep = x[:, 0]  # the quirky CLS token
+                logits_dict[str(i)] = self.aux_heads[str(i)](cls_rep)
 
-        main_logits = self.vit.forward_head(x)
+        logits_dict["final"] = self.vit.forward_head(x)
 
-        return main_logits, aux_logits
-        
-        
+        return logits_dict
+
+    def compute_loss(self, inputs, targets, aux_weight=0.5):
+        """
+        Computes loss for a batch (inputs, targets).
+        """
+
+        logits_dict = self.forward(x)
+        aux_loss = 0.0
+        for k, logits in logits_dict.items():
+            if k == "final":
+                main_loss = F.cross_entropy(logits, targets)
+            else:
+                aux_loss += F.cross_entropy(logits, targets)
+
+        if len(self.aux_layers) > 0:
+            loss = (
+                self.aux_weight * (aux_loss / len(self.aux_layers))
+                + (1 - self.aux_weight) * main_loss
+            )
+        else:
+            loss = main_loss
+
+        return loss
 
     @torch.no_grad()
-    def predict_with_early_exit(x,
-                                threshold=0.9):
+    def predict_with_early_exit(x, threshold=0.9):
         """
         Takes a timm ViT model and rRuns inference with early exits.
         - exit layers: list of layer indices where aux heads are attached.
         - threshold: confidence threshold for early exit
-    
+
         Returns:
         - predicted class
         - exit layer, or "final"
         - confidence score
-    
+
         We follow the form of the forward function of timm's ViT,
         specifically the "forward_features" function.
         https://github.com/huggingface/pytorch-image-models/blob/954613a470652e4a113ff45b62dbd15c4e229218/timm/models/vision_transformer.py#L934C9-L934C25
@@ -66,27 +87,26 @@ class ViTWithAuxHeads(nn.Module):
         x = self.vit._pos_embed(x)
         x = self.vit.patch_drop(x)
         x = self.vit.norm_pre(x)
-    
+
         # We skip the case of attention masking, not needed for
         # currently planned experiments.
         for i, block in enumerate(self.vit.blocks, start=1):
             x = block(x)
-    
+
             if i in exit_layers:
                 # It seems we need to do this because it's the only
                 # available representation at intermediate depths
-                cls_rep = x[:,0] # the quirky CLS token
+                cls_rep = x[:, 0]  # the quirky CLS token
                 logits = self.vit.aux_heads[str(i)](cls_rep)
                 probs = F.softmax(logits, dim=-1)
                 confidence, prediction = probs.max(dim=-1)
-    
+
                 if confidence.item() >= threshold:
                     return prediction.item(), i, confidence.item()
-    
+
         # If we didn't exit early...
         logits = self.vit.forward_head(x)
         probs = F.softmax(logits, dim=-1)
         confidence, prediction = probs.max(dim=-1)
-    
+
         return prediction.item(), "final", confidence.item()
-    
