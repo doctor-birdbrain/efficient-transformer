b062a0894b77632902d77f415089b273413ee3e7
Nikos Arechiga <nikos.arechiga@gmail.com>
2025-08-26 08:52:31 -0700

    Updated training loop, from what was in the collab notebook

--- train.py
+++ train.py
@@ -10,7 +10,6 @@ def train_and_log(
     model,
     train_dataset,
     val_dataset=None,
-    data_transform=None,
     num_steps=100,
     val_interval=10,
     batch_size=32,
@@ -28,10 +27,10 @@ def train_and_log(
     model = model.to(device)
 
     # Create data loader
-    dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    dataloader = DataLoader(train_dataset, batch_size=batch_size)
     if val_dataset:
-        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-        
+        val_loader = DataLoader(val_dataset, batch_size=batch_size)
+
 
     # Simple optimizer & loss
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
@@ -53,14 +52,16 @@ def train_and_log(
     # The file with the validation log
     os.makedirs(f"{local_log_path}", exist_ok=True)
     while step < num_steps:
+        # print("torch.cuda.memory_allocated: %fGB"%(torch.cuda.memory_allocated(0)/1024/1024/1024))
+        # print("torch.cuda.memory_reserved: %fGB"%(torch.cuda.memory_reserved(0)/1024/1024/1024))
+        # print("torch.cuda.max_memory_reserved: %fGB"%(torch.cuda.max_memory_reserved(0)/1024/1024/1024))
         try:
-            inputs, targets = next(data_iter)
+            batch = next(data_iter)
         except StopIteration:
             data_iter = iter(dataloader)
-            inputs, targets = next(data_iter)
-        if data_transform:
-            inputs = data_transform(inputs)
+            batch = next(data_iter)
 
+        inputs, targets = batch["image"], batch["label"]
         inputs, targets = inputs.to(device), targets.to(device)
 
         optimizer.zero_grad()
@@ -69,26 +70,15 @@ def train_and_log(
         loss.backward()
         optimizer.step()
 
-        metrics = {
-            "step": step,
-            "loss": loss.item()
-        }
-
-        append_dict_to_csv(f"{local_log_path}/{run_name}", metrics)
-            
-        # Log to W&B
-        wandb.log({"train/loss": loss.item(), "step": step})
-
          # Validation step
-        if val_dataset and ((step + 1) % val_interval == 0 or step == num_iterations - 1):
+        if val_dataset and ((step + 1) % val_interval == 0 or step == num_steps - 1):
             model.eval()
             val_loss = 0.0
             correct = 0
             total = 0
             with torch.no_grad():
-                for val_inputs, val_targets in val_loader:
-                    if data_transform:
-                        val_inputs = data_transform(val_inputs)
+                for val_batch in val_loader:
+                    val_inputs, val_targets = val_batch["image"], val_batch["label"]
                     val_inputs, val_targets = val_inputs.to(device), val_targets.to(device)
                     val_outputs = model(val_inputs)
                     v_loss = criterion(val_outputs, val_targets)
@@ -104,25 +94,36 @@ def train_and_log(
             metrics = {
                 "step": step,
                 "val/loss": avg_val_loss,
-                "val/accuracy": val_acc,      
+                "val/accuracy": val_acc,
+                "train/loss": loss.item()
             }
             wandb.log(metrics)
             append_dict_to_csv(f"{local_log_path}/{run_name}_val", metrics)
-            
+
             model.train()
-    
+        else:
+            metrics = {
+              "step": step,
+              "loss": loss.item()
+            }
+
+            append_dict_to_csv(f"{local_log_path}/{run_name}", metrics)
+
+            # Log to W&B
+            wandb.log({"train/loss": loss.item(), "step": step})
+
         step += 1
 
     wandb.finish()
 
 def append_dict_to_csv(file_path, data):
     file_exists = os.path.isfile(file_path) and os.path.getsize(file_path) > 0
-    
+
     with open(file_path, mode='a', newline='', encoding='utf-8') as f:
         writer = csv.DictWriter(f, fieldnames=data.keys())
-        
+
         # If file is empty, write header first
         if not file_exists:
             writer.writeheader()
-        
+
         writer.writerow(data)
